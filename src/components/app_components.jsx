282dd75a9dc947be3163f5fdc4937b4b1c6ce3fd
Kristine Laing <kristal.tasha@gmail.com>
2022-03-28 22:08:38 +0000

    landing header done

---
+++ src/components/app_components.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/app_components.css'
+
+function Header() {
+  return (
+    <header>
+      <div className='landing'>
+        <h3 className='land-title'>The Code Room</h3>
+      </div>
+    </header>
+  )
+}
+
+export default {
+  Header
+}
